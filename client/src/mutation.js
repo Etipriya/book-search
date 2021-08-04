4f7c70d6f269c5f471333c3b30c74234ccd89e0d
Eti Priya <etipriya@gmail.com>
2021-08-04 22:00:59 +0100

    wip client login and signup

---
+++ client/src/mutation.js
@@ -0,0 +1,27 @@
+import { gql } from "@apollo/client";
+
+export const LOGIN = gql`
+  mutation Mutation($loginInput: LoginInput!) {
+    login(input: $loginInput) {
+      token
+      user {
+        _id
+        email
+        username
+      }
+    }
+  }
+`;
+
+export const SIGNUP = gql`
+  mutation Mutation($addUserInput: AddUserInput!) {
+    addUser(input: $addUserInput) {
+      token
+      user {
+        _id
+        username
+        email
+      }
+    }
+  }
+`;
