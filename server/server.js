50aa9ebfbec559b018afe11435c42e5e7000cc3f
Eti Priya <etipriya@gmail.com>
2021-08-04 22:03:33 +0100

    added initial typedefs and resolvers in server.js

--- server/server.js
+++ server/server.js
@@ -1,21 +1,36 @@
-const express = require('express');
-const path = require('path');
-const db = require('./config/connection');
-const routes = require('./routes');
+const { ApolloServer } = require("apollo-server-express");
+const express = require("express");
+const path = require("path");
+const db = require("./config/connection");
+// const routes = require('./routes');
+
+const typeDefs = require("./schemas");
+const resolvers = require("./schemas");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+const startServer = async () => {
+  await server.start();
+  server.applyMiddleware({ app });
+};
+startServer();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
-app.use(routes);
+// app.use(routes);
 
-db.once('open', () => {
+db.once("open", () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
