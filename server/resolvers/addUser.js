fe2ad673c11de3dab4f839042366b2a774a05b75
Eti Priya <etipriya@gmail.com>
2021-08-05 15:28:48 +0100

    complete addUser file

--- server/resolvers/addUser.js
+++ server/resolvers/addUser.js
@@ -1,16 +1,24 @@
+const { AuthenticationError } = require("apollo-server-express");
 const { User } = require("../models/User");
+const { signToken } = require("../utils/auth");
 
 const addUser = async (_, { input }) => {
   const { username, email, password } = input;
 
-  const newUser = await User.create({ username, email, password });
+  const user = await User.findOne({ email });
 
-  if (!newUser) {
-    throw new AuthenticationError("Something is wrong");
+  if (!user) {
+    const newUser = await User.create({ username, email, password });
+
+    const token = signToken({
+      email: newUser.email,
+      id: newUser._id,
+      username: newUser.username,
+    });
+
+    return { token, user: newUser };
+  } else {
+    throw new AuthenticationError("User already exists");
   }
-
-  //TO Do token stuff
-  return { newUser };
 };
-
 module.exports = addUser;
