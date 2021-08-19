e4a4ec931829af134841bce30458730e2ea333fe
Eti Priya <etipriya@gmail.com>
2021-08-19 23:43:58 +0100

    added files in utils

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,28 +1,28 @@
 // route to get logged in user's info (needs the token)
-export const getMe = (token) => {
-  return fetch('/api/users/me', {
+export const getMe = token => {
+  return fetch("/api/users/me", {
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
       authorization: `Bearer ${token}`,
     },
   });
 };
 
-export const createUser = (userData) => {
-  return fetch('/api/users', {
-    method: 'POST',
+export const createUser = userData => {
+  return fetch("/api/users", {
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     },
     body: JSON.stringify(userData),
   });
 };
 
-export const loginUser = (userData) => {
-  return fetch('/api/users/login', {
-    method: 'POST',
+export const loginUser = userData => {
+  return fetch("/api/users/login", {
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     },
     body: JSON.stringify(userData),
   });
@@ -30,10 +30,10 @@ export const loginUser = (userData) => {
 
 // save book data for a logged in user
 export const saveBook = (bookData, token) => {
-  return fetch('/api/users', {
-    method: 'PUT',
+  return fetch("/api/users", {
+    method: "PUT",
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
       authorization: `Bearer ${token}`,
     },
     body: JSON.stringify(bookData),
@@ -43,7 +43,7 @@ export const saveBook = (bookData, token) => {
 // remove saved book data for a logged in user
 export const deleteBook = (bookId, token) => {
   return fetch(`/api/users/books/${bookId}`, {
-    method: 'DELETE',
+    method: "DELETE",
     headers: {
       authorization: `Bearer ${token}`,
     },
@@ -52,6 +52,6 @@ export const deleteBook = (bookId, token) => {
 
 // make a search to google books api
 // https://www.googleapis.com/books/v1/volumes?q=harry+potter
-export const searchGoogleBooks = (query) => {
+export const searchGoogleBooks = query => {
   return fetch(`https://www.googleapis.com/books/v1/volumes?q=${query}`);
 };
